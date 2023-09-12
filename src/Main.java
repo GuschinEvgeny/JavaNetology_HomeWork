5c9b3431a4cb915abc7b967a07a2967ff422e95a
Guschin Evgeny <eg@vidis.tech>
2023-09-12 13:16:12 +0300

    add Screenshot

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
+
     }
 }
